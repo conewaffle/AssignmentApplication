d6320b4786fb0e29d97a96d7d5cbb85357af26d5
conewaffle <kevinjm.liang@gmail.com>
2019-04-22 20:39:57 +1000

    continuing retrofit implementation

--- app/src/main/java/com/example/assignmentapplication/ResearchActivity.java
+++ app/src/main/java/com/example/assignmentapplication/ResearchActivity.java
@@ -8,8 +8,12 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SearchView;
 
 import com.example.assignmentapplication.research.AcademicResponse;
@@ -23,8 +27,12 @@ import java.util.List;
 
 public class ResearchActivity extends AppCompatActivity {
 
-    private SearchView searchView;
+    private EditText searchText;
+    private Button searchButton;
     private RecyclerView recyclerView;
+    private ResearchAdapter mAdapter;
+
+    private ProgressDialog progDialog;
 
 
     @Override
@@ -33,20 +41,43 @@ public class ResearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_research);
         setTitle("Search for Academic Resources");
 
-        searchView = findViewById(R.id.searchView);
+        progDialog = new ProgressDialog(ResearchActivity.this);
+
+        searchText = findViewById(R.id.searchText);
+        searchButton = findViewById(R.id.btnSearch);
         recyclerView = findViewById(R.id.searchRecycler);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        GetResourcesTask getResourcesTask = new GetResourcesTask();
-        getResourcesTask.execute();
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = searchText.getText().toString();
+
+                GetResourcesTask getResourcesTask = new GetResourcesTask();
+                getResourcesTask.execute(query);
+
+            }
+        });
+
+
 
     }
 
-    private class GetResourcesTask extends AsyncTask<Void, Void, ArrayList<Mod>>{
+    private class GetResourcesTask extends AsyncTask<String, Void, ArrayList<Mod>>{
 
         @Override
-        protected ArrayList<Mod> doInBackground(Void... voids) {
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progDialog.setMessage("Loading Topics...");
+            progDialog.setIndeterminate(false);
+            progDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progDialog.setCancelable(true);
+            progDialog.show();
+        }
+
+        @Override
+        protected ArrayList<Mod> doInBackground(String... query) {
 
             try{
                 Retrofit retrofit = new Retrofit.Builder()
@@ -56,7 +87,7 @@ public class ResearchActivity extends AppCompatActivity {
 
                 ResearchClient client = retrofit.create(ResearchClient.class);
 
-                Call<AcademicResponse> academicCall = client.getResponse("peanuts");
+                Call<AcademicResponse> academicCall = client.getResponse(query[0]);
                 Response<AcademicResponse> academicResponse = academicCall.execute();
                 ArrayList<Mod> myList = (ArrayList<Mod>) academicResponse.body().getItems().getMods();
 
@@ -70,8 +101,9 @@ public class ResearchActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(ArrayList<Mod> result){
-            //mAdapter = new ResearchAdapter(result);
-            //recyclerView.setAdapter(mAdapter);
+            mAdapter = new ResearchAdapter(result);
+            recyclerView.setAdapter(mAdapter);
+            progDialog.dismiss();
         }
     }
 
--- app/src/main/java/com/example/assignmentapplication/ResearchAdapter.java
+++ app/src/main/java/com/example/assignmentapplication/ResearchAdapter.java
@@ -1,4 +1,76 @@
 package com.example.assignmentapplication;
 
-public class ResearchAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.assignmentapplication.research.Mod;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ResearchAdapter extends RecyclerView.Adapter<ResearchAdapter.ResearchViewHolder>{
+
+    private ArrayList<Mod> mDataset;
+
+    public ResearchAdapter(ArrayList<Mod> myDataset){mDataset = myDataset;}
+
+    public class ResearchViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public TextView titleYear, author, pub, itemType;
+        public CardView researchCard;
+
+        public ResearchViewHolder(View itemView){
+            super(itemView);
+            titleYear = itemView.findViewById(R.id.researchTitle);
+            author = itemView.findViewById(R.id.researchAuthor);
+            pub = itemView.findViewById(R.id.researchPub);
+            itemType = itemView.findViewById(R.id.researchType);
+            researchCard = itemView.findViewById(R.id.cardResearch);
+
+            researchCard.setOnClickListener(this);
+        }
+
+        //finish getting link.
+        @Override
+        public void onClick(View view){
+            int position = getAdapterPosition();
+            Mod myMod = mDataset.get(position);
+            String s = myMod.getPhysicalDescription().getExtent();
+        }
+    }
+
+    @Override
+    public ResearchAdapter.ResearchViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View researchView = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_research_item, parent, false);
+        ResearchViewHolder vh = new ResearchViewHolder(researchView);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ResearchViewHolder holder, int position){
+        if (mDataset.get(position).getOriginInfo().getPublisher()!=null){
+            holder.pub.setText(mDataset.get(position).getOriginInfo().getPublisher());
+        } else holder.pub.setText("No Publisher");
+        if (mDataset.get(position).getTitleInfo().getTitle()!=null) {
+            holder.titleYear.setText(mDataset.get(position).getTitleInfo().getTitle());
+        } else holder.titleYear.setText("No Title");
+        if (mDataset.get(position).getPhysicalDescription().getExtent()!=null) {
+            holder.author.setText(mDataset.get(position).getPhysicalDescription().getExtent());
+        } else holder.author.setText("No Details");
+        if (mDataset.get(position).getTypeOfResource()!=null) {
+            holder.itemType.setText(mDataset.get(position).getTypeOfResource());
+        } else holder.itemType.setText("No Details");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+
 }
